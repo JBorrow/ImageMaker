e5079d8900480ccae673e6ff524bd5743ffef232
JBorrow <joshua.borrow@durham.ac.uk>
2014-07-04 21:07:29 +0100

    Initial ImageMaker.py commit

---
+++ ImageMaker.py
@@ -0,0 +1,145 @@
+'''
+Acts as a wrapper for Richard Bower's script that is called
+'make_standard_images.py'. This wrapper makes the script much
+easier to use as previously the actual file was needed to be
+each and every use.
+
+Important: running this script will create directories within
+your current one if neccessary, and the data will be stored
+as:
+
+/Webpage/<Object Number, Centre co-ords, Gallery>/Snapshot
+<snapshotnumber>/<size>
+
+Josh Borrow
+04/07/2014
+'''
+
+import numpy as N
+#import make_standard_images as msi
+
+#initialise variables
+
+snap_list = [] #list of snapshots that are required
+object_list = [] #list of objects that are required
+size_list = ['xsmall'] #sizes chosen from the styles DB
+position = N.array([0,0,0]) #position center
+types = ['normal'] #types of images, see documentation
+text = True #text e.g. position on image 
+deletefiles = True #cleans up after making gallery
+nfof = 50 #number of fof
+centre_fof = 0
+first_fof = 0
+fof_step = 1
+subsample = 1 #subsample factor to not read in all particles
+gas_cmap = None
+
+#just for rotations
+
+rotating = False #rotating image only works with normal/position
+
+
+#just for 'special' gallery - EXPERIMENTAL AND DOESN'T WORK YET
+
+remake_gallery = True #remake gallery of xsmall images
+minitype = 'gas' #type of the miniature images
+label_only = False #no minature images if True
+
+
+#questioning variables
+
+usertype = ""
+
+
+#questioning functions
+
+def askForUserType():
+	'''
+	Asks the user for their type and raises an exception if somehow broken. Also accepts 'exit' which
+	quits the program with exit code 1
+	'''
+	usertype = ""
+
+	while usertype not in ["b","a","exit"]:
+		usertype = raw_input("Would you like a basic options list or an advanced options list? (b/a): ")
+
+	if usertype == "b":
+		return "b"
+	elif usertype == "a":
+		return "a"
+	elif usertype == "exit":
+		print "Exiting..."
+		exit(1)
+	else:
+		raise InputError(msg = "I don't know how, but you broke me in the askForUserType() function")
+
+def askForImageType():
+	'''
+	Asks the user for the type of image required. The currently accepted types are:
+		xxsmall 				xxlarge 					app
+		xxsmall-2 				exhibition  				long_shot
+		xxsmall-gist 			exhibition2
+		xxsmall-linear 			exhibition_rotation
+		xsmall 					supersize
+		small					supersize_rotation
+		medium-hi 				supsersize_simple
+		medium 					custom
+		large 					xlarge_rotation
+		xlarge 					medium_rotation
+		
+	'''
+
+	possiblesizes = ["xxsmall" 			,	"xxlarge", 				"app",
+					 "xxsmall-2" 		,	"exhibition"  			"long_shot",
+					 "xxsmall-gist" 	,	"exhibition2",
+					 "xxsmall-linear" 	,	"exhibition_rotation",
+					 "xsmall"			,	"supersize",
+					 "small"			,	"supersize_rotation",
+					 "medium-hi" 		,	"supsersize_simple",
+					 "medium" 			,	"custom",
+					 "large" 			,	"xlarge_rotation",
+					 "xlarge" 			,	"medium_rotation"]
+
+	isokay = False #this is set to false initially, as we are going to perform checks based on it
+
+	while not isokay:
+		size_list = [] #for appending later
+		input = ""
+		correctlist = True
+
+		while input != "finished":
+			input = str(raw_input("What sizes do you require? Enter 'finished' once you are done: "))
+			if input != "finished":
+				size_list.append(input)
+
+		for size in size_list:
+			if size not in possiblesizes:
+				print '''Please try again. If you don't know the possible sizes, please consult 
+the documentation. Don't enter return characters! First bad size: ''', size
+				correctlist = False
+				break
+			else:
+				None
+
+		if correctlist == False:
+			continue
+		else:
+			isokay = True
+
+	return size_list
+
+
+
+#begin questioning
+
+#ask what type of experience they want
+
+usertype = askForUserType()
+
+size_list = askForImageType()
+
+
+#testing variables
+
+print 'Usertype: ', usertype
+print 'Size List: ', size_list, type(size_list)
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ImageMaker
 ==========
 
-ImageMaker stuff for the Eagle project
+The ImageMaker module creates some prompts to enter the stuff for creating images from the Eagle project.
+
+Original base code (make_standard_images.py) by Richard Bower, however this module was written by Joshua Borrow.
