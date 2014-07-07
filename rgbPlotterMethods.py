7ca378e04245b25a317165d84919acf2a33b5339
JBorrow <joshua.borrow@durham.ac.uk>
2014-07-07 13:49:06 +0100

    Finished imageAtPosition
    Nice and cleaned up now. Again, it’s very similar to Richard’s code
    however it does it in a much better format, and should be more
    efficient.

--- rgbPlotterMethods.py
+++ rgbPlotterMethods.py
@@ -124,4 +124,101 @@ class PlotterMethods:
                 centre_fof=my_fof)
 
 
-    def image_at_position():
+    def imageAtPosition(fileInfo, style=ImageStyles.xsmall, centre = [0.,0.,0.],
+        gasCmap = None, deltaAngle = 2., text=True):
+        '''
+        Make an image at some given position. Similar to above, except can be
+        rotating.
+        '''
+
+        # declare variables from style
+
+        width = style['width']
+        scale = N.array(style['scale'])
+        starScaleFactor = style['star_scale_factor']
+        darkScaleFactor = style['dark_scale_factor']
+        TLimits = N.array(style['T_limits'])
+
+
+        if 'pixels' in style.keys(): #image resolution
+            xPixels, yPixels = style['pixels']
+
+        else:
+            xPixels, yPixels = 1024, 1024
+
+
+        if 'use_logh' in style.keys(): #don't know what this does
+            useLogh = style['use_logh']
+
+        else:
+            useLogh = True
+
+
+        if 'text' in style.keys(): # overlay text e.g. position
+            useText = style['text']
+
+        else:
+            useText = True
+
+
+        if 'angle' in style.keys():
+            angle = style['angle']
+
+        else:
+            angle = 0
+
+
+        if not gasCmap:
+            gasCmap = style['gas_cmap'] #don't know what this does
+
+
+        if 'camera_z_distance' in style.keys():
+            #setting z distance and perspective plot
+            cameraZDistance = style['camera_z_distance']
+            perspective = True
+
+        else:
+            cameraZDistance = None
+            perspective = False
+
+
+        if 'opacity' in style.keys(): #set opacity of gas etc
+            opacity = style['opacity']
+
+        else:
+            opacity = 0
+
+
+
+
+        #now give these params to the eagle function
+
+        imageParams = eagle.ImageParams(width, scale, zoom=1., angle,
+        partplot=partplot, star_scale_factor = starScaleFactor,
+        dark_scale_factor = darkScaleFactor, gas_cmap = gasCmap,
+        T_limits = TLimits, use_logh = useLogh, perspective = perspective,
+        camera_z_distance = cameraZDistance, opacity = opacity)
+
+        plotParams = eagle.PlotParams(xpixels = xPixels, ypixels = yPixels,
+        text = useText)
+
+
+        #we need to get the data from eagle
+
+        #these extra params allow only a small subset to be found...
+
+        baseData = eagle.eagle_image_data(fileInfo, imageParams, plotParams)
+        baseData.ReadGroupData(surpress=False, centre=centre)
+
+        if not fileInfo.rotating:
+            print "Making images for position: ", centre
+            eagle.make_single_image(baseData)
+
+        else:
+            print "Making fof images - rotation for position: ", centre
+            #generate original image...
+            data = eagle.make_single_image(baseData)
+            #now make all the cool images around the position
+            for angle in N.arange(delta_angle,360., delta_angle):
+                baseData.imageParams.angle = angle
+                eagle.make_single_image(baseData, mydata=data)
