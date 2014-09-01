107eda1ecb011c16b6e22f4e8ff21cbbc8954a6d
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-09-01 18:15:59 +0100

    changed geometry to accept any image size

--- Image3D.py
+++ Image3D.py
@@ -22,7 +22,7 @@ class Image3D(object):
         self.rightDir = rightDir
         self.leftDir = leftDir
         self.saveDir = saveDir
-        self.saveres = saveRes 
+        self.saveRes = saveRes 
 
         print "Grabbing filenames from %s" % (rightDir)
         self.fileNameGrabber()
@@ -74,32 +74,72 @@ class Image3D(object):
         outputY = self.saveRes[1]
 
         # if images are bigger, crop
-        if inputX > outputX:
-            diffX = inputX - outputX
-            if diffX % 2 == 0:
-                leftCrop = diffX/2
-                rightCrop = inputX - diffX/2
-            else:
-                # give right the extra pixel
-                leftCrop = diffX/2
-                rightCrop = (inputX - (diffX/2)) - 1
-            
-        else:
-            rightCrop = inputX
-            leftCrop = 0
 
-        if inputY > outputY
-            diffY = inputY - outputY
-            if diffY % 2 == 0:
-                topCrop = diffY/2
-                bottomCrop = inputY - diffY/2
+        self.aspectRatio = float(outputX)/float(outputY)
+        
+        # is what we expect y to be smaller than what it is? if so crop in y
+        if inputX / self.aspectRatio < inputY:
+            diff = inputY - int(inputX / self.aspectRatio)
+            if diff % 2 == 0:
+                # set top (t) and bottom (b) pixles 'inside' the image
+                t = diff / 2
+                b = inputY - diff / 2
             else:
-                # give bottom the extra pixel
-                topCrop = diffY/2
-                bottomCrop = (inputY - (diffY/2)) - 1
-                
-        else:
-            topCrop = 0
-            bottomCrop = inputY 
+                # we have an extra pixel to deal with
+                t = diff / 2
+                b = (inputY - diff/2) - 1
 
+            l = 0
+            r = inputX
+        # is what we expect x to be smaller than what it is? if so crop x
+        elif inputY * self.aspectRatio < inputX:
+            diff = inputX - int(inputY * self.aspectRatio)
+            if diff % 2 == 0:
+                # set left (l) and right (r) pixels 'inside' the image
+                l = diff / 2
+                r = inputX - diff / 2
+            else:
+                # we have an extr pixel to deal with
+                l = diff / 2
+                r = (inputX - diff/2) - 1
+
+            t = 0
+            b = inputY
+        # if not caught by the above conditions, then it's already ok AR
+        else:
+            t = 0
+            r = inputX
+            l = 0
+            b = inputY
+        
+        cropBox = (l, t, r, b)
+
+        right.crop(cropBox)
+        left.crop(cropBox)
+        
+        # now we need to stretch them so they each fill half the canvas
+
+        if outputY % 2 == 0:
+            right.resize((outputX, outputY/2))
+            left.resize((outputX, outputY/2))
+        else:
+            # again, we have an extra pixel to play with
+            right.resize((outputX, outputY/2 + 1))
+            left.resize((outputX, outputY/2))
+
+        # we can add the pictures together now
+        
+        canvas = Image.new(1, self.saveRes)
+
+        # make sure left is top and right bottom or else headache
+
+        canvas.paste(left, (0,0))
+        canvas.paste(right, (0, (outputY / 2) + 1))
+        
+        canvas.show()
+        
         return
+
+if __name__ == "__main__":
+    image = Image3D()
+    image.imageTo3D("~/right/star", "~/left/star")
