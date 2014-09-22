e07e3d74edf3abaed1cbfbe190e917085048012e
joshua.borrow@durham.ac.uk <joshua.borrow@durham.ac.uk>
2014-09-22 13:10:56 +0100

    fixed indentation

--- RotationClass.py
+++ RotationClass.py
@@ -16,99 +16,103 @@ import copy
 from multiprocessing import Process
 
 class Rotation(ImageClass.Image):
-	'''Creates rotations around some point etc. tries to keep __init__
-	minimal'''
-	
-	def __init__(self, partplot = [True, True, True, True, True],
-	saveDir = "/cosma/home/mphf18/Data/test_images", gasCmap = None):
-		'''This is pretty much the exact same init as the class we inherit from.
-		This is kept minimal so we can give the user as much flexibility as
-		possible'''
-		
-		self.partplot = partplot
-		self.saveDir = saveDir
-		self.gasCmap = gasCmap
-		
-		return
+    '''Creates rotations around some point etc. tries to keep __init__
+    minimal'''
+    
+    def __init__(self, partplot = [True, True, True, True, True],
+    saveDir = "/cosma/home/mphf18/Data/test_images", gasCmap = None):
+    	'''This is pretty much the exact same init as the class we inherit from.
+    	This is kept minimal so we can give the user as much flexibility as
+    	possible'''
+    	
+    	self.partplot = partplot
+    	self.saveDir = saveDir
+    	self.gasCmap = gasCmap
+    	
+    	return
 
-	def makeObjectRotation(self, imageStyle = ImageStyles.xsmall,
-	snapNumber = 28, objectNumber = 0, text = True, nFrames = 360, nCores = 4,
-	cameraXDistance = 0, cameraZDistance = 10):
-		'''This requires a number of frames and number of cores, which have
-		defaults of 360 and 4 respecvitely. nFrames % nCores must be equal to 0
-		otherwise we will have missed frames
+    def makeObjectRotation(self, imageStyle = ImageStyles.xsmall,
+	snapNumber = 28, objectNumber = 0, text = True, nFrames = 360,
+        nCores = 4, cameraXDistance = 0, cameraZDistance = 10, position = -1):
+	'''This requires a number of frames and number of cores, which have
+    	defaults of 360 and 4 respecvitely. nFrames % nCores must be equal to 0
+    	otherwise we will have missed frames
 		
-		To make a 3-D rotation,. simply supply a cameraXDistance (reccomend
-		a 1:75 ratio of CXD : CZD'''
+	To make a 3-D rotation,. simply supply a cameraXDistance (reccomend
+	a 1:75 ratio of CXD : CZD'''
 
 	# first we get the 'baseData' which contains all the info for us to plot
-		self.objectNumber = objectNumber
-		self.snapNumber = snapNumber
-		self.imageStyle = imageStyle
-		self.cameraXDistance = cameraXDistance
+        if position != -1:
+            self.position = position
+        else:
+            self.objectNumber = objectNumber
+        #we do this so if they want a picture of a position we d that, otherwise
+        #we default to object number
+	self.snapNumber = snapNumber
+	self.imageStyle = imageStyle
+	self.cameraXDistance = cameraXDistance
+	self.imageStyleUnpack()
+	if cameraXDistance:
+	    # if they want to make a 3D image, set the z dist too
+	    self.cameraZDistance = cameraZDistance
 
-		self.imageStyleUnpack()
-		if cameraXDistance:
-			# if they want to make a 3D image, set the z dist too
-			self.cameraZDistance = cameraZDistance
+        #now we package
+	self.paramPack()
+	self.fileInfoPack(rotating = False)
 
-		#now we package
-		self.paramPack()
-		self.fileInfoPack(rotating = False)
+        self.baseDataGrabber()
+        
+        if nFrames % nCores != 0:
+            print "Sorry, you need to have nFrames % nCores == 0"
+            exit(-1)
 
-		self.baseDataGrabber()
-		
-		if nFrames % nCores != 0:
-			print "Sorry, you need to have nFrames % nCores == 0"
-			exit(-1)
+        step = 360 / (nFrames/nCores)
+        
+        for startAngle in range(0, 360, step):
+            jobs = []
+            for angle in range(startAngle, startAngle + step):
+                # we start some processes up for each angle and kill them after
+                # to keep the memory useage low
+                jobs.append(Process(target = self.makeImageFromBaseData,
+                args = (angle,)))
 
-		step = 360 / (nFrames/nCores)
-		
-		for startAngle in range(0, 360, step):
-			jobs = []
-			for angle in range(startAngle, startAngle + step):
-				# we start some processes up for each angle and kill them after
-				# to keep the memory useage low
-				jobs.append(Process(target = self.makeImageFromBaseData,
-				args = (angle,)))
+            for job in jobs:
+                job.start()
 
-			for job in jobs:
-				job.start()
+            for job in jobs:
+                job.join()
 
-			for job in jobs:
-				job.join()
+        return
 
-		return
+    def makeImageFromBaseData(self, angle = 0):
+        '''Uses self.baseData to create images of some given angle'''
+        thisData = copy.deepcopy(self.baseData)
+        originalSaveDir = thisData.fileInfo.savedir
+        thisData.imageParams.angle = angle
+        
+        if self.cameraXDistance:
+            for side in ("left", "right"):
+                thisData.fileInfo.savedir = originalSaveDir + "/" + side
+                self.ensureDir(thisData.fileInfo.savedir)
+                if side == "left":
+                    cXD = self.cameraXDistance
+                elif side == "right":
+                    cXD = -self.cameraXDistance
 
-	def makeImageFromBaseData(self, angle = 0):
-		'''Uses self.baseData to create images of some given angle'''
-		thisData = copy.deepcopy(self.baseData)
-		originalSaveDir = thisData.fileInfo.savedir
-		thisData.imageParams.angle = angle
-		
-		if self.cameraXDistance:
-			for side in ("left", "right"):
-				thisData.fileInfo.savedir = originalSaveDir + "/" + side
-				self.ensureDir(thisData.fileInfo.savedir)
-				if side == "left":
-					cXD = self.cameraXDistance
-				elif side == "right":
-					cXD = -self.cameraXDistance
+                currentData = copy.deepcopy(thisData) # we need to copy 
+                # otherwise we end up with double the rotation on the left eye
+                currentData.plot_image(perspective = True,
+                camera_x_distance = cXD,
+                camera_z_distance = self.cameraZDistance)
+        else:
+                thisData.plot_image()
 
-				currentData = copy.deepcopy(thisData) # we need to copy 
-				# otherwise we end up with double the rotation on the left eye
-				currentData.plot_image(perspective = True,
-				camera_x_distance = cXD,
-				camera_z_distance = self.cameraZDistance)
-		else:
-			thisData.plot_image()
+        thisData = None
 
-		thisData = None
-
-		return
+        return
 
 if __name__ == "__main__":
-	rot = Rotation()
-	rot.makeObjectRotation(imageStyle = ImageStyles.josh_3D,
-	snapNumber = 28, objectNumber = 9000, text = True, nFrames = 360,
-	nCores = 10, cameraXDistance = 0.15, cameraZDistance = 0)
+    rot = Rotation()
+    rot.makeObjectRotation(imageStyle = ImageStyles.josh_3D,
+    snapNumber = 28, objectNumber = 9000, text = True, nFrames = 360,
+    nCores = 10, cameraXDistance = 0.15, cameraZDistance = 0)
